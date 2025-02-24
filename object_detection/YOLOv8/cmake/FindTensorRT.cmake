24e8ed2a44b3c44dc6bbbf9aa7f58df36ab28eb1
loveleaves <2248652711@qq.com>
2025-02-24 23:56:20 +0800

    update cmake

--- object_detection/YOLOv8/cmake/FindTensorRT.cmake
+++ object_detection/YOLOv8/cmake/FindTensorRT.cmake
@@ -1,138 +1,81 @@
-# This module defines the following variables:
-#
-# ::
-#
-#   TensorRT_INCLUDE_DIRS
-#   TensorRT_LIBRARIES
-#   TensorRT_FOUND
-#
-# ::
-#
-#   TensorRT_VERSION_STRING - version (x.y.z)
-#   TensorRT_VERSION_MAJOR  - major version (x)
-#   TensorRT_VERSION_MINOR  - minor version (y)
-#   TensorRT_VERSION_PATCH  - patch version (z)
-#
-# Hints
-# ^^^^^
-# A user may set ``TensorRT_ROOT`` to an installation root to tell this module where to look.
-#
-set(_TensorRT_SEARCHES)
+## find tensorrt
+include(FindPackageHandleStandardArgs)
 
-if(TensorRT_ROOT)
-    set(_TensorRT_SEARCH_ROOT PATHS ${TensorRT_ROOT} NO_DEFAULT_PATH)
-    list(APPEND _TensorRT_SEARCHES _TensorRT_SEARCH_ROOT)
-endif()
+## 用户可以输入的TensorRT 搜索路径
+set(TensorRT_ROOT
+	""
+	CACHE
+	PATH
+	"TensorRT root directory")
 
-# appends some common paths
-set(_TensorRT_SEARCH_NORMAL
-        PATHS "/usr"
+## 设置TensorRT 搜索路径
+set(TensorRT_SEARCH_PATH
+  /usr/include/x86_64-linux-gnu
+  /usr/src/tensorrt
+  /usr/lib/x86_64-linux-gnu
+  ${TensorRT_ROOT}
 )
-list(APPEND _TensorRT_SEARCHES _TensorRT_SEARCH_NORMAL)
 
-# Include dir
-foreach(search ${_TensorRT_SEARCHES})
-    find_path(TensorRT_INCLUDE_DIR NAMES NvInfer.h ${${search}} PATH_SUFFIXES include)
+## 设置需要搜索的TensorRT 依赖库
+set(TensorRT_ALL_LIBS
+  nvinfer
+  nvinfer_plugin
+  nvonnxparser
+)
+
+## 提前设置后面需要用的变量
+set(TensorRT_LIBS_LIST)
+set(TensorRT_LIBRARIES)
+
+## 搜索头文件的路径
+find_path(
+  TensorRT_INCLUDE_DIR
+  NAMES NvInfer.h
+  PATHS ${TensorRT_SEARCH_PATH}
+)
+
+## 利用头文件路径下的version文件来设置TensorRT的版本信息
+if(TensorRT_INCLUDE_DIR AND EXISTS "${TensorRT_INCLUDE_DIR}/NvInferVersion.h")
+  file(STRINGS "${TensorRT_INCLUDE_DIR}/NvInferVersion.h" TensorRT_MAJOR REGEX "^#define NV_TENSORRT_MAJOR [0-9]+.*$")
+  file(STRINGS "${TensorRT_INCLUDE_DIR}/NvInferVersion.h" TensorRT_MINOR REGEX "^#define NV_TENSORRT_MINOR [0-9]+.*$")
+  file(STRINGS "${TensorRT_INCLUDE_DIR}/NvInferVersion.h" TensorRT_PATCH REGEX "^#define NV_TENSORRT_PATCH [0-9]+.*$")
+
+  string(REGEX REPLACE "^#define NV_TENSORRT_MAJOR ([0-9]+).*$" "\\1" TensorRT_VERSION_MAJOR "${TensorRT_MAJOR}")
+  string(REGEX REPLACE "^#define NV_TENSORRT_MINOR ([0-9]+).*$" "\\1" TensorRT_VERSION_MINOR "${TensorRT_MINOR}")
+  string(REGEX REPLACE "^#define NV_TENSORRT_PATCH ([0-9]+).*$" "\\1" TensorRT_VERSION_PATCH "${TensorRT_PATCH}")
+  set(TensorRT_VERSION_STRING "${TensorRT_VERSION_MAJOR}.${TensorRT_VERSION_MINOR}.${TensorRT_VERSION_PATCH}")
+endif()
+message("TensorRT version: ${TensorRT_VERSION_STRING}")
+
+## 搜索sample code的路径
+find_path(
+  TensorRT_SAMPLE_DIR
+  NAMES trtexec/trtexec.cpp
+  PATHS ${TensorRT_SEARCH_PATH}
+  PATH_SUFFIXES samples
+)
+
+## 依次搜索TensorRT依赖库
+foreach(lib ${TensorRT_ALL_LIBS} )
+  find_library(
+    TensorRT_${lib}_LIBRARY
+    NAMES ${lib}
+    PATHS ${TensorRT_SEARCH_PATH}
+  )
+  ## 存储TensorRT的依赖库变量
+  set(TensorRT_LIBS_VARS TensorRT_${lib}_LIBRARY ${TensorRT_LIBS_LIST})
+  ## 也是TensorRT的依赖库，存成list，方便后面用foreach
+  list(APPEND TensorRT_LIBS_LIST TensorRT_${lib}_LIBRARY)
 endforeach()
 
-if(NOT TensorRT_LIBRARY)
-    foreach(search ${_TensorRT_SEARCHES})
-        find_library(TensorRT_LIBRARY NAMES nvinfer ${${search}} PATH_SUFFIXES lib)
-        if(NOT TensorRT_LIB_DIR)
-            get_filename_component(TensorRT_LIB_DIR ${TensorRT_LIBRARY} DIRECTORY)
-        endif ()
-    endforeach()
-endif()
-
-if(NOT TensorRT_nvinfer_plugin_LIBRARY)
-    foreach(search ${_TensorRT_SEARCHES})
-        find_library(TensorRT_nvinfer_plugin_LIBRARY NAMES nvinfer_plugin ${${search}} PATH_SUFFIXES lib)
-    endforeach()
-endif()
-
-mark_as_advanced(TensorRT_INCLUDE_DIR)
-
-if(TensorRT_INCLUDE_DIR AND EXISTS "${TensorRT_INCLUDE_DIR}/NvInfer.h")
-    if(EXISTS "${TensorRT_INCLUDE_DIR}/NvInferVersion.h")
-        set(_VersionSearchFile "${TensorRT_INCLUDE_DIR}/NvInferVersion.h")
-    else ()
-        set(_VersionSearchFile "${TensorRT_INCLUDE_DIR}/NvInfer.h")
-    endif ()
-    file(STRINGS "${_VersionSearchFile}" TensorRT_MAJOR REGEX "^#define NV_TENSORRT_MAJOR [0-9]+.*$")
-    file(STRINGS "${_VersionSearchFile}" TensorRT_MINOR REGEX "^#define NV_TENSORRT_MINOR [0-9]+.*$")
-    file(STRINGS "${_VersionSearchFile}" TensorRT_PATCH REGEX "^#define NV_TENSORRT_PATCH [0-9]+.*$")
-
-    string(REGEX REPLACE "^#define NV_TENSORRT_MAJOR ([0-9]+).*$" "\\1" TensorRT_VERSION_MAJOR "${TensorRT_MAJOR}")
-    string(REGEX REPLACE "^#define NV_TENSORRT_MINOR ([0-9]+).*$" "\\1" TensorRT_VERSION_MINOR "${TensorRT_MINOR}")
-    string(REGEX REPLACE "^#define NV_TENSORRT_PATCH ([0-9]+).*$" "\\1" TensorRT_VERSION_PATCH "${TensorRT_PATCH}")
-    set(TensorRT_VERSION_STRING "${TensorRT_VERSION_MAJOR}.${TensorRT_VERSION_MINOR}.${TensorRT_VERSION_PATCH}")
-endif()
-
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(TensorRT REQUIRED_VARS TensorRT_LIBRARY TensorRT_INCLUDE_DIR VERSION_VAR TensorRT_VERSION_STRING)
+## 调用cmake内置功能，设置基础变量如xxx_FOUND
+find_package_handle_standard_args(TensorRT REQUIRED_VARS TensorRT_INCLUDE_DIR TensorRT_SAMPLE_DIR ${TensorRT_LIBS_VARS})
 
 if(TensorRT_FOUND)
-    set(TensorRT_INCLUDE_DIRS ${TensorRT_INCLUDE_DIR})
-
-    if(NOT TensorRT_LIBRARIES)
-        set(TensorRT_LIBRARIES ${TensorRT_LIBRARY})
-        if (TensorRT_nvinfer_plugin_LIBRARY)
-            list(APPEND TensorRT_LIBRARIES ${TensorRT_nvinfer_plugin_LIBRARY})
-        endif()
-    endif()
-
-    if(NOT TARGET TensorRT::TensorRT)
-        add_library(TensorRT INTERFACE IMPORTED GLOBAL)
-        add_library(TensorRT::TensorRT ALIAS TensorRT)
-    endif()
-
-    if(NOT TARGET TensorRT::nvinfer)
-        add_library(TensorRT::nvinfer SHARED IMPORTED)
-        if (WIN32)
-            foreach(search ${_TensorRT_SEARCHES})
-                find_file(TensorRT_LIBRARY_DLL
-                        NAMES nvinfer.dll
-                        PATHS ${${search}}
-                        PATH_SUFFIXES bin
-                )
-            endforeach()
-
-            set_target_properties(TensorRT::nvinfer PROPERTIES
-                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_INCLUDE_DIRS}"
-                    IMPORTED_LOCATION "${TensorRT_LIBRARY_DLL}"
-                    IMPORTED_IMPLIB "${TensorRT_LIBRARY}"
-            )
-        else()
-            set_target_properties(TensorRT::nvinfer PROPERTIES
-                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_INCLUDE_DIRS}"
-                    IMPORTED_LOCATION "${TensorRT_LIBRARY}"
-            )
-        endif()
-        target_link_libraries(TensorRT INTERFACE TensorRT::nvinfer)
-    endif()
-
-    if(NOT TARGET TensorRT::nvinfer_plugin AND TensorRT_nvinfer_plugin_LIBRARY)
-        add_library(TensorRT::nvinfer_plugin SHARED IMPORTED)
-        if (WIN32)
-            foreach(search ${_TensorRT_SEARCHES})
-                find_file(TensorRT_nvinfer_plugin_LIBRARY_DLL
-                        NAMES nvinfer_plugin.dll
-                        PATHS ${${search}}
-                        PATH_SUFFIXES bin
-                )
-            endforeach()
-
-            set_target_properties(TensorRT::nvinfer_plugin PROPERTIES
-                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_INCLUDE_DIRS}"
-                    IMPORTED_LOCATION "${TensorRT_nvinfer_plugin_LIBRARY_DLL}"
-                    IMPORTED_IMPLIB "${TensorRT_nvinfer_plugin_LIBRARY}"
-            )
-        else()
-            set_target_properties(TensorRT::nvinfer_plugin PROPERTIES
-                    INTERFACE_INCLUDE_DIRECTORIES "${TensorRT_INCLUDE_DIRS}"
-                    IMPORTED_LOCATION "${TensorRT_nvinfer_plugin_LIBRARY}"
-            )
-        endif()
-        target_link_libraries(TensorRT INTERFACE TensorRT::nvinfer_plugin)
-    endif()
+  ## 设置Tensor_LIBRARIES变量
+  foreach(lib ${TensorRT_LIBS_LIST} )
+    list(APPEND TensorRT_LIBRARIES ${${lib}})
+  endforeach()
+  message("Found TensorRT: ${TensorRT_INCLUDE_DIR} ${TensorRT_LIBRARIES} ${TensorRT_SAMPLE_DIR}")
+  message("TensorRT version: ${TensorRT_VERSION_STRING}")
 endif()
